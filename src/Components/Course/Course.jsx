809ed27210c1426fc4987c07988648602518199e
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-05-25 12:29:06 +0600

    button fixed by using flex grow

--- src/Components/Course/Course.jsx
+++ src/Components/Course/Course.jsx
@@ -5,28 +5,29 @@ const Course = ({ course,handleCourseTitle ,handleAllFunctionalities}) => {
     // console.log(course)
     const { course_credit, course_title, cover_img, description, price } = course;
     return (
-        <div className="bg-slate-100 rounded-xl max-h-screen">
+        <div className="bg-slate-100 rounded-xl max-h-screen flex flex-col">
             <div className="p-5"><img className="rounded-lg w-full h-40" src={cover_img} alt="" /></div>
             <div className="px-5  my-1">
                 <h1 className="text-2xl font-semibold ">
                     {course_title}
                 </h1>
-                <p className="py-1 h-24">
+                <p className="py-1 flex-grow">
                     {
                         description
                     }
                 </p>
             </div>
-           <div className='h-32'>
+           <div className='flex-grow'>
            <div className="px-5 flex justify-between pt-3 text-2xl">
                 <p>bdt {price}</p>
                 <p>{course_credit} hours</p>
             </div>
-          <div className="mx-5 mt-5 mb-3  bg-blue-600 rounded text-center">
+          
+           </div>
+           <div className="mx-5 mt-5 mb-3  bg-blue-600 rounded text-center">
           <button onClick={()=>{handleAllFunctionalities(course)}} className="py-2 text-center text-3xl ">Submit</button>
          
           </div>
-           </div>
         </div>
     );
 };
